ab4f1dd521bad0a8305ae9b092e857d01fc16f9f
AJaiman <aravjaiman@outlook.com>
2023-12-13 12:55:47 -0500

    remove test prints

--- scanner.py
+++ scanner.py
@@ -10,18 +10,22 @@ def isSus(filePath):
 
 def scanDirectory(path):
     files = []
+    
     for root, dirs, fs in os.walk(path):
         files.append([root, fs])
-    print(files, "\n")
+
     for pair in files:
         fileList = pair[1]
         newFileList = []
+
         for file in fileList:
             n = file.split(".")
+
             if n[-1] in susTypes:
                 newFileList.append(file)
+
         pair[1] = newFileList
-    print(files)
+
     #susFiles = []
     #return susFiles
 
