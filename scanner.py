7895f083e1556b212a10caf8af69dfb6c7e8e50e
AJaiman <aravjaiman@outlook.com>
2023-12-13 12:55:27 -0500

    FInd files with suspicious file extensions

--- scanner.py
+++ scanner.py
@@ -2,9 +2,27 @@ import os
 
 #os.chdir(path) sets working directory
 #os.stat(path) gets file stats
+#os.walk() will give you the root, dirs, and files in a direcotry (including in all the subdirs)
+susTypes = ["exe", "dll", "bat", "ps1"]
+
 def isSus(filePath):
     return False
 
 def scanDirectory(path):
-    susFiles = []
-    return susFiles
+    files = []
+    for root, dirs, fs in os.walk(path):
+        files.append([root, fs])
+    print(files, "\n")
+    for pair in files:
+        fileList = pair[1]
+        newFileList = []
+        for file in fileList:
+            n = file.split(".")
+            if n[-1] in susTypes:
+                newFileList.append(file)
+        pair[1] = newFileList
+    print(files)
+    #susFiles = []
+    #return susFiles
+
+scanDirectory("C:\\Users\\aravj\\Downloads")
